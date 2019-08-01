a870ca1b5f3adf09f1fc028602b3bd0fe11e0844
Pravar Agrawal <pravarag@gmail.com>
2019-08-01 21:00:24 +0530

    Add screen-manager files

---
+++ main.py
@@ -0,0 +1,34 @@
+import pyautogui
+import time
+# import keyboard
+import os
+
+
+def manage_screens():
+    counter = 0
+    screen_dir = '/Users/pravar_ag/Python_Work/git_clones/screen-manager/screens'
+    while counter == 0:
+        print("Inside counter")
+        time.sleep(5)
+        print("Sleep was executed")
+        # if keyboard.is_pressed('i'):
+        if os.getcwd() == screen_dir:
+            take_screens()
+
+        else:
+            take_screens()
+            os.chdir(os.getcwd()+'/screens')
+
+        time.sleep(10)
+
+
+def take_screens():
+    image = pyautogui.screenshot()
+    img_name = pyautogui.screenshot()
+    img_name = "screen_"+str(time.time())+".png"
+    image.save(img_name)
+
+
+if __name__ == '__main__':
+
+    manage_screens()
